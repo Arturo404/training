5fc7bd5d92dc9e2605cd3760b91c1aa3c93ff384
Arturo <arthursouss@gmail.com>
2024-02-07 18:38:13 +0200

    Correcting after Hava PR

--- JSTraining/typescript/client.ts
+++ JSTraining/typescript/client.ts
@@ -1,23 +1,25 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import * as dotenv from "dotenv";
-import { Credentials } from './credentials.mjs';
+import { Credentials } from './credentials.js';
+import Logger from 'js-logger';
 
 dotenv.config();
 
-const port = process.env.PORT || 3000;
-const url_base = `http://localhost:${port}`;
+const port: number = Number(process.env.PORT) || 3000;
+const hostname: string = process.env.HOSTNAME || 'localhost';
+const url_base: string = `http://${hostname}:${port}`;
 
-const client = axios.create({
+const client: AxiosInstance = axios.create({
     baseURL: url_base
 });
 
-const signUp = async (username: string, password: string) => {
+const signUp = async (username: string, password: string) : Promise<void> => {
     try {
         const {data} = await client.post('/signup', new Credentials(username, password));
-        console.log(data);
+        Logger.info(data);
     }
     catch(err: any) {
-        console.error(`Client request error: ${err.message}`);
+        Logger.error(`Client request error: ${err.message}`);
     }
 }
 
--- JSTraining/typescript/server.ts
+++ JSTraining/typescript/server.ts
@@ -1,44 +1,46 @@
 import express, { Request, Response } from 'express';
-import { Credentials } from './credentials.mjs';
+import { Credentials } from './credentials.js';
 import bodyParser from 'body-parser';
+import Logger from 'js-logger';
 
-let credentials: Credentials[]  = []
+const credentials: Credentials[]  = [];
 
 const app = express();
 app.use(bodyParser.json());
 
 const port: number = Number(process.env.PORT) || 3000;
-const hostName: string = `localhost`;
+const hostName: string = process.env.HOSTNAME || `localhost`;
 
 app.post('/signup', (req: Request, res: Response) => {
-    console.log("received request");
+    Logger.debug("received request");
     const userCredentials: Credentials = req.body;
     for(const credential of credentials) {
-        if(credential.username == userCredentials.username) {
-            if(credential.password == userCredentials.password) {
-                console.log("Existing credentials: given credentials are correct.");
-                res.send("Existing credentials: given credentials are correct.");
+        if(credential.username === userCredentials.username) {
+            if(credential.password === userCredentials.password) {
+                res.send("Username found: given credentials are correct.");
                 return;
             }
             else {
-                console.log("Existing credentials: given credentials are incorrect.");
-                res.send("Existing credentials: given credentials are incorrect.");
+                res.send("Username found: incorrect password.");
                 return;
             }
         }
+        else {
+
+        }
     }
 
-    credentials.push(new Credentials(userCredentials.username, userCredentials.password));
-    console.log("Non-existing credentials: new credentials have been registered.");
-    res.send("Non-existing credentials: new credentials have been registered.");
+    const newCredentials = new Credentials(userCredentials.username, userCredentials.password);
+    credentials.push(newCredentials);
+    res.send("Username not found: new credentials have been registered.");
     return;
 });
 
 try {
     app.listen(port,  hostName, () => {
-        console.log(`Server running at ${hostName}:${port}`);
+        Logger.info(`Server running at ${hostName}:${port}`);
     });
 }
 catch(err: any) {
-    console.log(`Error initializing server: ${err.message}`)
+    Logger.error(`Error initializing server: ${err.message}`)
 }
